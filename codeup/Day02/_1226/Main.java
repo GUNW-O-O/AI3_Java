e41fc2d577951249e94ef73736202fd377c2635f
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-14 18:24:56 +0900

    5

--- codeup/Day02/_1226/Main.java
+++ codeup/Day02/_1226/Main.java
@@ -6,14 +6,69 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+//        for (int i=0; i<=lotto.length; i++) {
+//        	if (lotto[i] == hyeji[0]) {
+//        		count++;
+//        	}
+//        }
 		
-		
-		int arr1[] = new int [7];
-		int arr2[] = new int [6];
-		// arr 2차원 배열에 정수를 입력받아보세요.
-		
-		
-		
-		
+		// 첫 번째 행렬은 7개의 숫자
+        int[] lotto = new int[7];
+        // 두 번째 행렬은 6개의 숫자
+        int[] hyeji = new int[6];
+
+
+        // 첫 번째 행렬에 7개 숫자 입력받기
+        for (int i = 0; i < 7; i++) {
+        	lotto[i] = sc.nextInt();
+        }
+
+        // 두 번째 행렬에 6개 숫자 입력받기
+        for (int i = 0; i < 6; i++) {
+        	hyeji[i] = sc.nextInt();
+        }
+        int count = 0;
+        int bonus = 0;
+        
+        for (int i =0; i<=6; i++ ) {
+        	for (int j : hyeji) {
+				if(i==j) {
+					count++;
+				}
+			}
+		}
+        int bonusLotto = lotto[lotto.length - 1];
+        for (int i : hyeji) {
+			if (bonusLotto == i) {
+				bonus++;
+			}
+		}
+        
+        
+        switch (count) {
+		case 6:System.out.print(1);
+			break;
+		case 5:System.out.print(3);
+			break;
+		case 4:System.out.print(4);
+			break;
+		case 3:System.out.print(5);
+			break;
+
+		default:System.out.print(0);
+			break;
+		}
+        if (count==6) return;
+        switch (count+bonus) {
+		case 6:
+			System.out.print(2);
+			break;
+
+		default:
+			break;
+		}
+       
+       
+       
 	}
 }
---
+++ codeup/Day03/_1503/Main.java
@@ -0,0 +1,36 @@
+package _1503;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a = 1;
+		int arr[][] = new int [n][n];
+		
+		boolean sw= true;
+		for(int i = 0; i < arr.length; i++) {
+			if(sw)
+				for (int j = 0; j<arr.length; j++)
+					arr[i][j] = a++;
+			if (!sw)
+				for ( int j = n-1 ; j>=0; j--)
+					arr[i][j] = a++;
+			sw = !sw;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+
+}
