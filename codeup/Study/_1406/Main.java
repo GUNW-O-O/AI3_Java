573913d7064b49875daa4b10da6899631d75e70a
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-18 17:57:08 +0900

    7

---
+++ codeup/Study/_1406/Main.java
@@ -0,0 +1,20 @@
+package _1406;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		String b = "love";
+		
+		if (a.equals(b)) {
+			System.out.print("I love you.");
+		}
+		
+		sc.close();
+		
+	}
+}
