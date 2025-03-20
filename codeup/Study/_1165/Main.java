c1891dee41682ea2151d96586404b96c0aea6c3f
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-20 11:54:04 +0900

    code up

---
+++ codeup/Study/_1165/Main.java
@@ -0,0 +1,21 @@
+package _1165;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int goal = b;
+		int end = 0;
+		int time = 90 - a; //4
+		
+		if ( time %5 ==0) end = b + time/5 ;
+		else end = b + time/5 +1;
+		System.out.print(end);
+		sc.close();
+	}
+}
---
+++ codeup/Study/_1166/Main.java
@@ -0,0 +1,21 @@
+package _1166;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if (a%400 == 0) {
+			System.out.print("Leap");
+		}
+		else if (a%4 ==0 && a%100!=0) {
+			System.out.print("Leap");
+		}
+		else System.out.print("Normal");
+			
+		sc.close();
+	}
+}
