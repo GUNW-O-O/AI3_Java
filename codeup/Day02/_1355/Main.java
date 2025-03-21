ed9767a2421752749fe0ed7b1a1d5e2fee7cc22a
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-21 10:15:00 +0900

    code up

--- codeup/Day02/_1355/Main.java
+++ codeup/Day02/_1355/Main.java
@@ -9,19 +9,16 @@ public class Main {
         int a = sc.nextInt();
 
         for (int i = 1; i <= a; i++) {
-            // 왼쪽 공백 출력
             for (int j = 1; j < i; j++) {
-                System.out.print(" ");  // 공백 출력
+                System.out.print(" ");
             }
-
-            // 별 출력
             for (int j = 1; j <= a - i + 1; j++) {
-                System.out.print("*");  // 별 출력
-            }
+                System.out.print("*");  
 
-            System.out.println();  // 한 줄 출력 후 줄 바꿈
-        }        
+            System.out.println();
+        }     
 
         sc.close();
     }
+}
 }
