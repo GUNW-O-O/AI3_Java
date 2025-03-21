de618a0310361e295e7a8b6597353b8a5639755e
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-21 18:19:42 +0900

    3.21

--- codeup/Study/_1356/Main.java
+++ codeup/Study/_1356/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int empty = a-2;
-		for (int i = 1; i <= a; i++) {
-			for (int j = 1; j <= a-2; j++) {
-				System.out.print("*");
-				if (i <= 2 && i <= a-1 ) {
-					for (int k = 0; k < j; k++) {
-						System.out.print(" ");
-					}
-				}
-				 
-			}System.out.println();
-		}
-		
-	}
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
 }
