af277dcbb87d765260e270e187148f2b3f5d9523
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-26 18:21:08 +0900

    3.26

--- Java/src/Day11/Ex01_Generics/BoxEx.java
+++ Java/src/Day11/Ex01_Generics/BoxEx.java
@@ -10,6 +10,12 @@ public class BoxEx {
 		// - 예시 : class Box <T> { }
 		// - 생성 : Box<타입> box = new Box<타입> ();
 		
+		Box<?> box;
+		
+		box = new Box<String>();
+		box = new Box<Integer>();
+		box = new Box<Pikachu>();
+		
 		Box<String> box1 = new Box<String>();
 		box1.setT("물");
 		String boxName = box1.getT();
