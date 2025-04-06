002dd64248566c46baeae485c05c35a59cb5d7c6
GUNW-O-O <rhrjsdn12@naver.com>
2025-04-06 20:53:01 +0900

    sort

---
+++ Java/src/book/sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package book.sort;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int temp = 0;
+        for ( int i = 0; i < arr.length-1; i++) {
+            for ( int j = 0; j < arr.length-1-i; j++) {
+                if ( arr[j] > arr[j+1] ) {
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+
+}
---
+++ Java/src/book/sort/SelectSort.java
@@ -0,0 +1,41 @@
+package book.sort;
+
+import java.util.Scanner;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for(int i = 0; i < arr.length; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+int temp = 0;
+int minind = 0;
+boolean sw = false;
+        for( int i = 0; i < arr.length-1; i++) {
+            for( int j = i; j < arr.length-1; j++ ) {
+                if ( arr[i] > arr[j+1] ) {
+                    temp = arr[i];
+                    if ( arr[j+1] < arr[minind] ) {
+                        minind = j + 1;
+                    }
+                    sw = true;
+                }
+            }
+            if (sw == true) {
+                arr[i] = arr[minind];
+                arr[minind] = temp;
+            }
+            sw = false;
+        }
+
+        for(int i = 0; i < arr.length-1; i++) {
+            System.out.println(arr[i]);
+        }
+
+
+
+        sc.close();
+    }
+
+}
